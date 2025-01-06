27b52022f7a972d4bc83449f4f4418c6d1406b00
SofiaShein <sheinkinasofiya@gmail.com>
2025-01-06 18:06:23 +0200

    Initial commit

---
+++ src/main/java/task9/Apartment.java
@@ -0,0 +1,104 @@
+package task9;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "apartments")
+public class Apartment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column (nullable = false)
+    private String district;
+
+    @Column (nullable = false)
+    private String address;
+
+    @Column (nullable = false)
+    private double area;
+
+    @Column (nullable = false)
+    private int rooms;
+
+    @Column (nullable = false)
+    private double price;
+
+    public Apartment() {
+    }
+
+    public Apartment(String district, String address, double area, int rooms, double price) {
+        this.district = district;
+        this.address = address;
+        this.area = area;
+        this.rooms = rooms;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Apartments{" +
+                "id=" + id +
+                ", district='" + district + '\'' +
+                ", address='" + address + '\'' +
+                ", area=" + area +
+                ", rooms=" + rooms +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/task9/ApartmentsService.java
@@ -0,0 +1,49 @@
+package task9;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+@Service
+public class ApartmentsService {
+    private final ApartmentsRepository apartmentsRepository;
+
+
+    public ApartmentsService(ApartmentsRepository apartmentsRepository) {
+        this.apartmentsRepository = apartmentsRepository;
+    }
+
+
+    @Transactional(readOnly = true)
+    public long count() {
+        return apartmentsRepository.count();
+    }
+
+    @Transactional
+    public void addApartment(Apartment apartment) {
+        apartmentsRepository.save(apartment);
+    }
+
+    public void deleteApartment(long[] idList) {
+        for (long id : idList) {
+            apartmentsRepository.deleteById(id);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Apartment> findAll(Pageable pageable) {
+        return apartmentsRepository.findAll(pageable).getContent();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Apartment> findByPrice (double bottom, double top, Pageable pageable) {
+        return apartmentsRepository.findByPrice(bottom, top, pageable);
+    }
+
+    @Transactional
+    public void updateApartment(String district, String address, double area, int rooms, double price, long id) {
+        apartmentsRepository.updateApartment(district, address, area, rooms, price, id);
+    }
+
+}
